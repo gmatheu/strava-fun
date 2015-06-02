645bb303f08f720dc3f5e168def3522602071ad2
Gonzalo Matheu <gonzalommj@gmail.com>
2015-06-02 20:38:27 -0300

    Creating lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to strava-fun
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/strava_fun/core.clj
@@ -0,0 +1,7 @@
+(ns strava-fun.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
